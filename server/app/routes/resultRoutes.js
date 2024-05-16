87847020103e7333ea44af9e8eb05744f3819e05
adamkuzlo <adamkuzlo619@gmail.com>
2024-05-16 06:43:31 -0400

    result route module export

--- server/app/routes/resultRoutes.js
+++ server/app/routes/resultRoutes.js
@@ -1,34 +1,36 @@
-// Import the express module and the result controller
-const express = require("express");
-const result = require("../controllers/result.controller.js");
+module.exports = (app) => {
+  // Import the express module and the result controller
+  const express = require("express");
+  const result = require("../controllers/result.controller.js");
 
-// Create an instance of the express Router
-const router = express.Router();
+  // Create an instance of the express Router
+  const router = express.Router();
 
-/**
- * GET request to retrieve all results
- *
- * @returns {Array} An array of all result objects
- */
-router.get("/get-all", result.findAll);
+  /**
+   * GET request to retrieve all results
+   *
+   * @returns {Array} An array of all result objects
+   */
+  router.get("/get-all", result.findAll);
 
-/**
- * GET request to retrieve a single result by its ID
- *
- * @param {number} id - The ID of the result to retrieve
- *
- * @returns {Object} The result object with the matching ID
- */
-router.get("/get-one/:id", result.findAllById);
+  /**
+   * GET request to retrieve a single result by its ID
+   *
+   * @param {number} id - The ID of the result to retrieve
+   *
+   * @returns {Object} The result object with the matching ID
+   */
+  router.get("/get-one/:id", result.findAllById);
 
-/**
- * POST request to create a new result
- *
- * @param {Object} req.body - The result data to be stored
- *
- * @returns {Object} The newly created result object
- */
-router.post("/create", result.create);
+  /**
+   * POST request to create a new result
+   *
+   * @param {Object} req.body - The result data to be stored
+   *
+   * @returns {Object} The newly created result object
+   */
+  router.post("/create", result.create);
 
-// Export the router to be used in the main application
-app.use("/api/result", router);
+  // Export the router to be used in the main application
+  app.use("/api/result", router);
+};
